0404ba6c458251577ba0eeb6afb4a529b6063490
Tim <Tim@MacBook-Pro-de-Tim.local>
2019-10-30 15:41:36 +0100

    add componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import DisplayEmployee from './components/DisplayEmployee';
 import './App.css';
 
-function App() {
+const sampleEmployee = {
+  gender: 'male',
+  name: {
+    first: 'Charlie',
+    last: 'Thompson',
+  },
+  location: {
+    street: {
+      number: 761,
+      name: 'Tay Street',
+    },
+    city: 'Timaru',
+    postcode: 76111,
+  },
+  email: 'charlie.thompson@example.com',
+  picture: {
+    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
+  },
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      employee: null
+    }
+    this.getEmployee = this.getEmployee.bind(this);
+  };
+
+  getEmployee() {
+    // Send the request
+    axios.get('https://randomuser.me/api?nat=fr')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        console.log(data)
+        this.setState({
+          employee: data.results[0],
+        });
+    });
+  }
+
+  componentDidMount() {
+    this.getEmployee()
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+      this.state.employee
+      ?<DisplayEmployee employee={this.state.employee} /> : <p>No data yet</p>}
+      <button type="button" onClick={this.getEmployee}>Get employee</button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
